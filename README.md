2f2df1cfbd8245822055c30bb36680113d45d233
Gerardo Custodio <everythingissofast@gmail.com>
2026-06-15 07:33:25 -0600

    add img to readme

---
+++ README.md
@@ -0,0 +1,11 @@
+## Aplicacion para iOS Iphone
+
+### Busqueda en una lista de elementos
+
+* Utilice Xcode 26 para desarrollarla, tambien Git y Github para el versionado del codigo.
+
+## 📱 Vista Previa
+![Vista previa searchable lists](./assets/img/searchable_lists.png "vista previa searchable list")
+
+
+
--- SearchableLists/ContentView.swift
+++ SearchableLists/ContentView.swift
@@ -8,6 +8,7 @@
 import SwiftUI
 
 struct ContentView: View {
+    
     @State private var searchText = ""
     let food: [Food] = Food.sampleFood
     
@@ -15,19 +16,19 @@ struct ContentView: View {
         NavigationStack {
             List {
                 ForEach(searchResults, id: \.self) { food in
-                    LabeledContent(food.name) { Text("\(food.category.rawValue)")}
+                    LabeledContent(food.name) { Text("\(food.category.rawValue)") }
                 }
             }
+            .searchable(text: $searchText, prompt: "Search for a food")
+            .navigationTitle("Foods")
         }
-        .searchable(text: $searchText, prompt: "Search for a food")
-        navigationTitle("Foods")
     }
     
     var searchResults: [Food] {
         if searchText.isEmpty {
             return food
         } else {
-            return food.filter { $0.name.lowercased().contains(searchText.lowercased())}
+            return food.filter { $0.name.lowercased().contains(searchText.lowercased()) }
         }
     }
 }
